bf3dc3cf3a70d0366e4373556b7871897c47b824
cliffmin <cliffmin@gmail.com>
2014-08-25 14:13:39 -0700

    added whisker and time plots

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -13,12 +13,13 @@ angular.module('tmsApp')
                         aquaxTimes: data['aquaxTimes'],
                         weekActivities: data['weekActivities'],
                         instrumentOrtActivities: data['instrumentOrtActivities'],
-                        ortWeek: data['ortWeek']
+                        ortWeek: data['ortWeek'],
+                        possiblePasses: data['possiblePasses']
                     },
                     adjustments: function(item) {
                         var size = Math.min(18, item.size);
                         return {
-                            y: item.y + size * 0.02,
+                            y: item.y + size * 0.04,
                             size: size * 0.9
                         };
                     },
@@ -107,7 +108,7 @@ angular.module('tmsApp')
                             layers: [{
                                 type: 'symbol',
                                 color: 'blue',
-                                shape: 'diamond',
+                                shape: 'triangle-down',
                                 mappings: function(d) {
                                     return {
                                         x: utc(d.start)
@@ -150,7 +151,7 @@ angular.module('tmsApp')
                                 }
                             }, {
                                 type: 'label',
-                                anchor: 'right',
+                                anchor: 'left',
                                 mappings: function(d) {
                                     return {
                                         x: utc(d.start),
@@ -165,7 +166,7 @@ angular.module('tmsApp')
                                 }
                             }, {
                                 type: 'label',
-                                anchor: 'left',
+                                anchor: 'right',
                                 mappings: function(d) {
                                     return {
                                         x: utc(d.end),
@@ -182,28 +183,64 @@ angular.module('tmsApp')
                         }, {
                             title: 'Instrument ORT Activities',
                             from: 'instrumentOrtActivities',
+                            layout: Timely.Layouts.staircase,
                             layers: [{
-                                type: 'rect',
-                                mappings: function(d) {
-                                    return {
-                                        x: utc(d.start),
-                                        x2: utc(d.end),
+                                    type: 'rect',
+                                    mappings: function(d) {
+                                        return {
+                                            x: utc(d.start),
+                                            x2: utc(d.end),
+                                        }
                                     }
-                                }
-                            },{
-                                type: 'label',
-                                mappings: function(d){
-                                    return {
-                                                       x: utc(d.start),
-                                        x2: utc(d.end),
-                                        text: d.text
+                                }, {
+                                    type: 'label',
+                                    mappings: function(d) {
+                                        return {
+                                            x: utc(d.start),
+                                            x2: utc(d.end),
+                                            text: d.text
+                                        }
+                                    },
+                                    adjustments: function(d) {
+                                        return {
+                                            // Slightly shrink the start/end times relative to the main labels
+                                            size: Math.min(12, d.size * 0.6)
+                                        };
                                     }
-                                }
-                            }
-
+                                }, {
+                                    type: 'label',
+                                    anchor: 'left',
+                                    mappings: function(d) {
+                                        return {
+                                            x: utc(d.start),
+                                            text: utc(d.start)
+                                        }
+                                    },
+                                    adjustments: function(d) {
+                                        return {
+                                            // Slightly shrink the start/end times relative to the main labels
+                                            size: Math.min(12, d.size * 0.4)
+                                        };
+                                    },
+                                }, {
+                                    type: 'label',
+                                    anchor: 'right',
+                                    mappings: function(d) {
+                                        return {
+                                            x: utc(d.end),
+                                            text: utc(d.end)
+                                        }
+                                    },
+                                    adjustments: function(d) {
+                                        return {
+                                            // Slightly shrink the start/end times relative to the main labels
+                                            size: Math.min(12, d.size * 0.4)
+                                        };
+                                    },
+                                },
 
                             ]
-                        },{
+                        }, {
                             title: 'Science ORT Week 26 auto',
                             from: 'ortWeek',
                             layers: [{
@@ -240,6 +277,87 @@ angular.module('tmsApp')
                                     };
                                 }
                             }]
+                        }, {
+                            title: 'Science ORT all possible passes named ALL',
+                            from: 'possiblePasses',
+                            layout: Timely.Layouts.staircase,
+                            adjustments: function(d) {
+                                return {
+                                    // Slightly shrink the start/end times relative to the main labels
+                                    size: Math.min(12, d.size * 0.8)
+                                };
+                            },
+                            layers: [{
+                                type: 'rect',
+                                stroke: 'white',
+                                mappings: function(d) {
+                                    return {
+                                        x: utc2(d.start1),
+                                        x2: utc2(d.end1),
+                                        fill: d.color
+                                    };
+                                }
+                            }, {
+                                type: 'label',
+                                mappings: function(d) {
+                                    return {
+                                        x: utc2(d.start1),
+                                        x2: utc2(d.end1),
+                                        text: d.text,
+                                        fill: d.color
+                                    };
+                                }
+                            }, {
+                                type: 'label',
+                                fill: 'none',
+                                anchor: 'right',
+                                mappings: function(d) {
+                                    return {
+                                        text: utc2(d.start1),
+                                        x: utc2(d.start1),
+                                    };
+                                }
+                            }, {
+                                type: 'label',
+                                fill: 'none',
+                                anchor: 'left',
+                                mappings: function(d) {
+                                    return {
+                                        text: utc2(d.end1),
+                                        x: utc2(d.end1),
+                                    };
+                                }
+                            },{
+                                type: 'whisker',
+                                fill: 'none',
+                                mappings: function(d) {
+                                    return {
+                                        x: utc2(d.start2),
+                                        x2: utc2(d.end2)
+                                    };
+                                }
+                            },{
+                                type: 'label',
+                                fill: 'none',
+                                anchor: 'right',
+                                mappings: function(d) {
+                                    return {
+                                        text: utc2(d.start2),
+                                        x: utc2(d.start2),
+                                    };
+                                }
+                            }, {
+                                type: 'label',
+                                fill: 'none',
+                                anchor: 'left',
+                                mappings: function(d) {
+                                    return {
+                                        text: utc2(d.end2),
+                                        x: utc2(d.end2),
+                                    };
+                                }
+                            }
+                            ]
                         }
 
                     ]
@@ -288,6 +406,13 @@ angular.module('tmsApp')
                 return parser.parse(d);
             };
         })();
+        var utc2 = (function() {
+            var parser = d3.time.format.utc('%Y%j%H%M%S');
+            return function(d) {
+                return parser.parse(d);
+            };
+        })();
+
 
 
     });
